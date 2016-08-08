6e88e36078768fc0a8801a01966e46f069f7447b
pentel13 <bmorelli25@gmail.com>
2016-08-08 16:15:19 -0700

    testing rendered todo list items
    Using testutils and scry rendered components with type to compare array
    length and determine if correct number of todo items are being rendered

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,24 @@ var expect = require('expect');
 var $ = require('jquery');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+
+  it('should render one Todo component for each Todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Check mail'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    
+    expect(todosComponents.length).toBe(todos.length);
   });
 });
