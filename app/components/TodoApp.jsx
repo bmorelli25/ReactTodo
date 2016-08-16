b1f60cd7de057148ca9b5bbee747781cde29a12a
pentel13 <bmorelli25@gmail.com>
2016-08-16 15:12:59 -0700

    Using momentjs to add created and completed dates
    Each todo now has a created at date. Completed todos show a completed at
    date

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -25,7 +26,9 @@ var TodoApp = React.createClass({
         {
           id: uuid(),
           text: text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
@@ -34,6 +37,7 @@ var TodoApp = React.createClass({
     var updatedTodos = this.state.todos.map(function (todo) {
       if (id === todo.id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
       return todo;
     });
