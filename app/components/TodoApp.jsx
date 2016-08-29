87c0100eedab9f1a4d9eba049f3c7576b84227ff
pentel13 <bmorelli25@gmail.com>
2016-08-29 13:54:12 -0700

    Removing old TodoApp.jsx code
    No longer need a lot of the functions in TodoApp as they are handled
    elsewhere with Redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -5,54 +5,18 @@ var moment = require('moment');
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    };
-  },
-  componentDidUpdate: function () { //gets fired after either props or state changes
-    TodoAPI.setTodos(this.state.todos);
-  },
-  //listen for new Todo items being created
-  handleAddTodo: function (text){
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(),
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    });
-  },
-  handleSearch: function (showCompleted, searchText) {
-    //we get the search text and the checkbox status from TodoSearch.jsx
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  },
   render: function () {
-    var {todos, showCompleted, searchText} = this.state;
-    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-
     return (
       <div>
         <h1 className='page-title'>Todo App</h1>
         <div className='row'>
           <div className='column small-centered small-11 medium-6 large-5'>
             <div className='container'>
-              <TodoSearch onSearch={this.handleSearch}/>
+              <TodoSearch/>
               <TodoList/>
-              <AddTodo onAddTodo={this.handleAddTodo}/>
+              <AddTodo/>
             </div>
           </div>
         </div>
