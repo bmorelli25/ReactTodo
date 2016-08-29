d990b42129e66ebe70c547b7a3722bb5cf05eebc
pentel13 <bmorelli25@gmail.com>
2016-08-29 13:50:39 -0700

    Removing default actions from app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -12,10 +12,6 @@ store.subscribe(() => {
   console.log('New State: ', store.getState());
 });
 
-store.dispatch(actions.addTodo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
-
 //Load foundation
 $(document).foundation();
 
