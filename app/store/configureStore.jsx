9e232638af0a4af794b553b7b3a1c0468c99a694
pentel13 <bmorelli25@gmail.com>
2016-09-08 10:08:11 -0700

    installing and applying redux-thunk

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -1,6 +1,7 @@
-var redux = require('redux');
+import * as redux from 'redux';
+import thunk from 'redux-thunk';
 
-var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
+import {searchTextReducer, showCompletedReducer, todosReducer} from 'reducers';
 
 export var configure = (initialState) => {
   var reducer = redux.combineReducers({
@@ -10,6 +11,7 @@ export var configure = (initialState) => {
   });
 
   var store = redux.createStore(reducer, initialState, redux.compose(
+    redux.applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ));
 
