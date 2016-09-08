441d235dfd18b1d5c2f19d5007687e41e5f7eeed
pentel13 <bmorelli25@gmail.com>
2016-09-08 10:30:01 -0700

    Fixed failing todosReducer test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -29,11 +29,16 @@ describe('Reducers', () => {
     it('should add new todo', () => {
       var action = {
         type: 'ADD_TODO',
-        text: 'New Todo Text'
+        todo: {
+          id: 'abc123',
+          text: 'Somthing to do',
+          completed: false,
+          createdAt: 123123
+        }
       };
       var res = reducers.todosReducer(df([]), df(action));
       expect(res.length).toEqual(1);
-      expect(res[0].text).toEqual(action.text);
+      expect(res[0]).toEqual(action.todo);
     });
 
     it('should toggle todo', () => {
