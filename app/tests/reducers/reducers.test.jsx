212b7bafae4656a589e1626ec9c59ea77cf0eb8a
pentel13 <bmorelli25@gmail.com>
2016-09-09 06:53:18 -0700

    Fixed failing test: should toggle todo

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -41,20 +41,26 @@ describe('Reducers', () => {
       expect(res[0]).toEqual(action.todo);
     });
 
-    it('should toggle todo', () => {
+    it('should update todo', () => {
       var todos = [{
         id: '123',
         completed: true,
         createdAt: 123,
         completedAt: 125
       }];
+      var updates = {
+        completed: false,
+        createdAt: null
+      };
       var action = {
-        type: 'TOGGLE_TODO',
-        id: '123',
+        type: 'UPDATE_TODO',
+        id: todos[0].id,
+        updates
       };
       var res = reducers.todosReducer(df(todos), df(action));
-      expect(res[0].completed).toEqual(false);
-      expect(res[0].completedAt).toEqual(undefined);
+      expect(res[0].completed).toEqual(updates.completed);
+      expect(res[0].completedAt).toEqual(updates.completedAt);
+      expect(res[0].text).toEqual(todos[0].text);
     });
 
     it('should add existing todos', () => {
