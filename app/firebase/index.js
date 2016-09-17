6a27c9d92699eeb28b0e3c6df620c5b3d9101d84
pentel13 <bmorelli25@gmail.com>
2016-09-17 10:06:09 -0700

    Fixing Firebase Fatal Error
    Fixing bug in last commit

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -6,7 +6,10 @@ try {
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
     storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
+
+  console.log(config);
 
   firebase.initializeApp(config);
 } catch (e) {
