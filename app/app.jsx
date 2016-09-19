3cf00e0855107389d79cf43c11b185da50b2e09a
pentel13 <bmorelli25@gmail.com>
2016-09-18 18:41:15 -0700

    Setting up React Router
    Using React Router to route new Login component

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-//var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
-
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
+import Login from 'Login';
 
 // store.subscribe(() => {
 //   var state = store.getState();
@@ -30,7 +30,12 @@ require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp/>
+    <Router history={hashHistory}>
+      <Route path="/">
+        <Route path="todos" component={TodoApp}/>
+        <IndexRoute component={Login}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
