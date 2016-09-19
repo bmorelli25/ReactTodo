5950f38257bb5ac00432dcd7c6a26e82c79bdc63
pentel13 <bmorelli25@gmail.com>
2016-09-18 19:04:25 -0700

    Setting up Login Action
    Login with popup

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,4 +1,4 @@
-import firebase, {firebaseRef} from 'app/firebase/';
+import firebase, {firebaseRef, githubProvider} from 'app/firebase/';
 import moment from 'moment';
 
 export var setSearchText = (searchText) => {
@@ -87,5 +87,21 @@ export var startToggleTodo = (id, completed) => {
     return todoRef.update(updates).then( () => {
       dispatch(updateTodo(id, updates));
     });
+  };
+};
+
+export var startLogin = () => {
+  return(dispatch, getState) => {
+    return firebase.auth().signInWithPopup(githubProvider).then(() => {
+      console.log('Auth worked!', result);
+    }, (error) => {
+      console.log('Unable to Auth', error);
+    };
+  };
+};
+
+export var startLogout = () => {
+  return(dispatch, getState) => {
+
   };
 };
--- app/firebase/index.js
+++ app/firebase/index.js
@@ -16,5 +16,6 @@ try {
 
 }
 
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 export var firebaseRef = firebase.database().ref();
 export default firebase;
