cf39dbc83f200ddb09803d8ef0a42d548872db4a
pentel13 <bmorelli25@gmail.com>
2016-09-18 18:46:15 -0700

    Adding to Login Component
    Utilizing Foundation to make things look a little better

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 export var Login = React.createClass({
   render() {
-    return <p>Login Component Rendering</p>;
+    return(
+      <div>
+        <h1 className='page-title'>React Todo App</h1>
+        <div className='row'>
+          <div className='columns small-centered small-10 medium-6 large-4'>
+            <div className='callout callout-auth'>
+              <h3>Login</h3>
+              <p>
+                Login with GitHub account below.
+              </p>
+              <button className='button'>Login with GitHub</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 });
 
