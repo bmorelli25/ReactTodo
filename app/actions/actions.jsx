88f554179dc4090376aec4d1c80ac36f784356f2
pentel13 <bmorelli25@gmail.com>
2016-09-18 19:14:43 -0700

    Wiring up Login.jsx to handle login action

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -92,11 +92,11 @@ export var startToggleTodo = (id, completed) => {
 
 export var startLogin = () => {
   return(dispatch, getState) => {
-    return firebase.auth().signInWithPopup(githubProvider).then(() => {
+    return firebase.auth().signInWithPopup(githubProvider).then((result) => {
       console.log('Auth worked!', result);
     }, (error) => {
       console.log('Unable to Auth', error);
-    };
+    });
   };
 };
 
--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import * as Redux from 'react-redux';
+
+import * as actions from 'actions';
 
 export var Login = React.createClass({
+  onLogin() {
+    var {dispatch} = this.props;
+
+    dispatch(actions.startLogin());
+  },
   render() {
     return(
       <div>
@@ -12,7 +20,7 @@ export var Login = React.createClass({
               <p>
                 Login with GitHub account below.
               </p>
-              <button className='button'>Login with GitHub</button>
+              <button className='button' onClick={this.onLogin}>Login with GitHub</button>
             </div>
           </div>
         </div>
@@ -21,4 +29,4 @@ export var Login = React.createClass({
   }
 });
 
-export default Login;
+export default Redux.connect()(Login);
