0bface406b7d1c0d00ea7e5f72abaf6d515729d3
pentel13 <bmorelli25@gmail.com>
2016-09-20 11:29:34 -0700

    Fixing auth vuln & new test to ensure it works
    On logout wasn't clearing state. Now returning empty array as state on
    logout to ensure data gets wiped.

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -82,6 +82,23 @@ describe('Reducers', () => {
       expect(res.length).toEqual(1);
       expect(res[0]).toEqual(todos[0]);
     });
+
+    it('should delete todos on Logout', () => {
+      var todos = [{
+        id: 111,
+        text: 'anything',
+        completed: false,
+        completeAt: undefined,
+        createdAt: 33000
+      }];
+
+      var action = {
+        type: 'LOGOUT'
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res.length).toEqual(0);
+    });
   });
 
   describe('authReducer', () => {
